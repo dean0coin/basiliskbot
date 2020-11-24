479c78e258a0e1969ffe640f35da869e21b088b4
dean0coin <69442911+dean0coin@users.noreply.github.com>
2020-11-24 04:39:20 -0500

    Update basilisk.py

--- basilisk.py
+++ basilisk.py
@@ -1,4 +1,4 @@
-import discord, random
+import discord, random, pygeoip
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Your ping is: {round(client.latency * 1000)}ms')
+    await ctx.send(f'Bot Latency: {round(client.latency * 1000)}ms')
 
 @client.command(aliases=['8ball', 'eightball', 'Eightball', 'EightBall'])
 async def _8ball(ctx, *, question):
@@ -90,7 +90,7 @@ async def source(ctx):
     await ctx.send("You can check out this bots source code at "+githublink)
 
 @client.command()
-async def cb(ctx):
+async def bsaycli(ctx):
     if (ctx.message.author.permissions_in(ctx.message.channel).administrator):
         message = input("Message to send: ")
         await ctx.send(message)
@@ -103,11 +103,15 @@ async def help(ctx):
     await ctx.send('''
             **Help Menu**
             ```Commands:
-            .ping - Check your Latency with Basilisk.
-            .8Ball (question) - Will respond with an answer to your question.
-            .flip - Coin Flip (Heads/Tails)
-            .plang - Random Programming Langauge
+            .ping - Check Basilisks Latency.
+            .8Ball (question) - Will respond with an answer to your question. -- Aliases: .eightball, .Eightball, .EightBall
+            .flip - Coin Flip (Heads/Tails).
+            .plang - Random Programming Langauge.
+            .ip2 (IP) - Performs IP Lookup. -- Aliases: .ipscan, .iplookup, .iplook
+            .skid - Outputs random skiddie quote. -- Aliases: .skiddie, .scriptkiddie
+            .test - Used to test if bot is online.
             .source - Link to this bots GitHub Page.
+            Rock Paper Scissors: .rock/.paper/.scissors - Plays Rock Paper Scissors with you.
             .help - This Menu.
             ```
     ''')
@@ -127,8 +131,87 @@ async def plang(ctx):
     
 @client.command()
 async def vortex(ctx):
-    await ctx.send(':heart:')
+    invChance = [0, 1]
+    invSend = random.choice(invChance)
+    if invSend == 0:
+        await ctx.send(':heart:')
+    elif invSend == 1:
+        await ctx.send(':heart:', 'https://discord.gg/dprJ49eXX4', ':heart:')
 
+@client.command()
+async def test(ctx):
+    await ctx.send("Bot is Online!")
+
+@client.command(aliases=['ipscan', 'iplookup', 'iplook'])
+async def ip2(ctx, ip):
+    gip = pygeoip.GeoIP('GeoLiteCity.dat')
+    res = gip.record_by_addr(ip)
+    for key,val in res.items():
+	    await ctx.send('%s : %s' % (key,val))
+
+@client.command()
+async def bsay(ctx, message=""):
+    await ctx.channel.purge(limit=1)
+    await ctx.send(message)
+
+@client.command(aliases=['skiddie', 'scriptkiddie'])
+async def skid(ctx):
+    skidSaysLst = ['can somebody help me with Hydra-Gtk please', 'guys I found this ddos script it DDoSes at like 100KB/s',
+    'whats programming?', 'who wants my cracking pack?', 'giving away a free credit card', 'whats a botnet?',
+    'im not a kid! wait whats a skid then?', 'just cracked 20 new renegade raiders', 'bro ill legit fry your router',
+    'what botnet do I use? Stressthem ofc', 'stop laughing just tell me what a linux is', 
+    'ofc i use windows what else is there? mac haha', 'how is an operating system california??', 'i use windows btw',
+    'whats an arch and why do you keep telling us you use it?', 'ugh this script stopped working',
+    'whats a python?', 'whats a java? can you send me the script']
+    skidSays = random.choice(skidSaysLst)
+    await ctx.send(skidSays)
+
+#---------------Rock Paper Scissors---------------#
+@client.command()
+async def rock(ctx):
+    rpsC = "Rock"
+    rps = ["Rock", "Paper", "Scissors"]
+    rpsS = random.choice(rps)
+    if rpsS == rpsC:
+        await ctx.send(rpsS)
+        await ctx.send("Tie!")
+    elif rpsS == "Paper":
+        await ctx.send(rpsS)
+        await ctx.send("lol u suck")
+    elif rpsS == "Scissors":
+        await ctx.send(rpsS)
+        await ctx.send("bro ur haxxing wtf")
+
+@client.command()
+async def paper(ctx):
+    rpsC = "Paper"
+    rps = ["Rock", "Paper", "Scissors"]
+    rpsS = random.choice(rps)
+    if rpsS == rpsC:
+        await ctx.send(rpsS)
+        await ctx.send("Tie!")
+    elif rpsS == "Scissors":
+        await ctx.send(rpsS)
+        await ctx.send("lol u suck")
+    elif rpsS == "Rock":
+        await ctx.send(rpsS)
+        await ctx.send("bro ur haxxing wtf")
+
+@client.command()
+async def scissors(ctx):
+    rpsC = "Scissors"
+    rps = ["Rock", "Paper", "Scissors"]
+    rpsS = random.choice(rps)
+    if rpsS == rpsC:
+        await ctx.send(rpsS)
+        await ctx.send("Tie!")
+    elif rpsS == "Rock":
+        await ctx.send(rpsS)
+        await ctx.send("lol u suck")
+    elif rpsS == "Paper":
+        await ctx.send(rpsS)
+        await ctx.send("bro ur haxxing wtf")
+#---------------Rock Paper Scissors---------------#
 client.run(TOKEN)
 
 
